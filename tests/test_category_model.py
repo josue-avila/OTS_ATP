364a39cc4adc1d1e32a39f803baefa799fc9f23e
josue-avila <josue.avila@olist.com>
2021-01-29 09:59:19 -0300

    adding coner cases (different types on category objects) to cases

--- tests/test_category_model.py
+++ tests/test_category_model.py
@@ -1,10 +1,23 @@
 from models.categories import Category
+from models.base_model import BaseModel
 
 
 class TestCategoryModel:
-    def test_name_isinstance_str(self):
+    def test_category_instance(self):
+        category = Category('test name', 'test description')
+        assert isinstance(category, Category)
+        assert isinstance(category, BaseModel)
+
+    def test_name_instance_none(self):
         try:
             Category(None, 'test description')
+            raise NotImplementedError('Not implemented error')
+        except Exception as error:
+            assert isinstance(error, TypeError)
+
+    def test_name_not_instance_str(self):
+        try:
+            Category(5, 'test description')
             raise NotImplementedError('Not implemented error')
         except Exception as error:
             assert isinstance(error, TypeError)
@@ -23,9 +36,16 @@ class TestCategoryModel:
         except Exception as error:
             assert isinstance(error, ValueError)
 
-    def test_description_isinstance_str(self):
+    def test_description_instance_none(self):
         try:
             Category('test name', None)
+            raise NotImplementedError('Not implemented error')
+        except Exception as error:
+            assert isinstance(error, TypeError)
+
+    def test_description_not_instance_str(self):
+        try:
+            Category('test name', 5)
             raise NotImplementedError('Not implemented error')
         except Exception as error:
             assert isinstance(error, TypeError)
